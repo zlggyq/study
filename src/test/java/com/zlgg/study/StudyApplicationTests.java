f2c21710b1762dc0c14b6589a8643607c4523658
zlgg <zlgg2020918@163.com>
2021-12-22 18:55:11 +0800

    es配置

--- src/test/java/com/zlgg/study/StudyApplicationTests.java
+++ src/test/java/com/zlgg/study/StudyApplicationTests.java
@@ -1,9 +1,20 @@
 package com.zlgg.study;
 
+import cn.hutool.core.thread.ThreadUtil;
 import com.zlgg.study.fastdfs.FastdfsUtils;
+import org.elasticsearch.action.ActionListener;
+import org.elasticsearch.action.admin.indices.alias.Alias;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.indices.CreateIndexRequest;
+import org.elasticsearch.client.indices.CreateIndexResponse;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.xcontent.XContentType;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
 
 @SpringBootTest
 class StudyApplicationTests {
@@ -11,10 +22,74 @@ class StudyApplicationTests {
     @Autowired
     FastdfsUtils fastdfsUtils;
 
-    @Test
-    void contextLoads() {
-        System.out.println(123);
+    @Autowired
+    RestHighLevelClient client;
 
+    @Test
+    void test() throws IOException {
+        // 创建索引
+        createIndex();
     }
 
+    /**
+     * 创建索引
+     */
+    private void createIndex() throws IOException {
+        // 1、创建 创建索引request 参数：索引名mess
+        CreateIndexRequest request = new CreateIndexRequest("zlgg");
+        // 2、设置索引的settings
+        request.settings(Settings.builder().put("index.number_of_shards", 3) // 分片数
+                .put("index.number_of_replicas", 2) // 副本数
+                .put("analysis.analyzer.default.tokenizer", "ik_max_word") // 默认分词器
+        );
+        // 3、设置索引的mappings
+        request.mapping(
+                "{\n" +
+                        "    \"properties\": \n" +
+                        "    {\n" +
+                        "      \"message\": \n" +
+                        "      {\n" +
+                        "        \"type\": \"text\"\n" +
+                        "      }\n" +
+                        "    }\n" +
+                        "  }"
+                , XContentType.JSON);
+        // 4、 设置索引的别名
+        request.alias(new Alias("zlgg2"));
+
+        // 5、 发送请求
+        // 5.1 同步方式发送请求
+        /*CreateIndexResponse createIndexResponse = client.indices()
+                .create(request, RequestOptions.DEFAULT);
+
+        // 6、处理响应
+        print(createIndexResponse);*/
+
+        // 5.2 异步方式发送请求
+        ActionListener<CreateIndexResponse> listener = new ActionListener<CreateIndexResponse>() {
+            @Override
+            public void onResponse(CreateIndexResponse createIndexResponse) {
+                // 6、处理响应
+                print(createIndexResponse);
+            }
+            @Override
+            public void onFailure(Exception e) {
+                System.out.println("创建索引异常：" + e.getMessage());
+            }
+        };
+        client.indices().createAsync(request, RequestOptions.DEFAULT, listener);
+        ThreadUtil.sleep(5000);
+    }
+
+    /**
+     * 打印结果
+     * @param createIndexResponse
+     */
+    private void print(CreateIndexResponse createIndexResponse) {
+        boolean acknowledged = createIndexResponse.isAcknowledged();
+        boolean shardsAcknowledged = createIndexResponse
+                .isShardsAcknowledged();
+        System.out.println("acknowledged = " + acknowledged);
+        System.out.println("shardsAcknowledged = " + shardsAcknowledged);
+    }
 }
